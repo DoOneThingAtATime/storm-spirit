ab73114428f5750fc0eff76f5693ad9b39c4540c
DoOneThingAtATime <guakun@outlook.com>
2019-06-28 17:45:55 +0800

    封装 eharts

---
+++ src/components/Chart.vue
@@ -0,0 +1,55 @@
+<template>
+  <div ref="chartDom"></div>
+</template>
+
+<script>
+import echarts from 'echarts'
+import debounce from 'lodash/debounce'
+import { addListener, removeListener } from 'resize-detector'
+
+export default {
+  props: {
+    option: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  watch: {
+    // option(val) {
+    //   this.chart.setOption(this.options)
+    // }
+    option: {
+      handler(val) {
+        console.log('gua')
+        this.chart.setOption(val)
+      },
+      deep: true
+    }
+  },
+  created() {
+    this.resize = debounce(this.resize, 370)
+  },
+  mounted() {
+    this.renderChart()
+    addListener(this.$refs.chartDom, this.resize)
+  },
+  beforeDestroy() {
+    removeListener(this.$refs.chartDom, this.resize)
+    this.chart.dispose()
+    this.chart = null
+  },
+  methods: {
+    resize() {
+      console.log('resize')
+      this.chart.resize()
+    },
+    renderChart() {
+      this.chart = echarts.init(this.$refs.chartDom)
+      this.chart.setOption(this.option)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- src/views/Dashboard/Analysis.vue
+++ src/views/Dashboard/Analysis.vue
@@ -1,13 +1,52 @@
 <template>
-  <div>Analysis</div>
+  <div>
+    Analysis
+    <charts style="height: 400px;" :option="chartOption"></charts>
+  </div>
 </template>
 
 <script>
+import Charts from '../../components/Chart'
 export default {
-
+  components: {
+    Charts
+  },
+  data() {
+    return {
+      chartOption: {
+        title: {
+          text: 'ECharts 入门示例'
+        },
+        tooltip: {},
+        xAxis: {
+          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+        },
+        yAxis: {},
+        series: [
+          {
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+          }
+        ]
+      }
+    }
+  },
+  mounted() {
+    this.interval = setInterval(() => {
+      // this.chartOption.series[0].data[0] = Math.floor(Math.random() * 100)
+      this.chartOption.series[0].data = this.chartOption.series[0].data.map(
+        () => Math.floor(Math.random() * 100)
+      )
+      console.log(1)
+      console.log(this.chartOption.series[0].data)
+    }, 1700)
+  },
+  beforeDestroy() {
+    clearInterval(this.interval)
+  }
 }
 </script>
 
 <style>
-
 </style>
