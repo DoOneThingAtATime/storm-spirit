367c29b3af3b1aa9bd1ec7064d839c2e342a5bb3
DoOneThingAtATime <guakun@outlook.com>
2019-07-03 17:49:07 +0800

    mock data

--- src/views/Dashboard/Analysis.vue
+++ src/views/Dashboard/Analysis.vue
@@ -7,11 +7,13 @@
 
 <script>
 import Charts from '../../components/Chart'
+import axios from 'axios'
+
 export default {
   components: {
     Charts
   },
-  data() {
+  data () {
     return {
       chartOption: {
         title: {
@@ -32,16 +34,38 @@ export default {
       }
     }
   },
-  mounted() {
+  mounted () {
+    this.getChartData()
     this.interval = setInterval(() => {
-      // this.chartOption.series[0].data[0] = Math.floor(Math.random() * 100)
-      this.chartOption.series[0].data = this.chartOption.series[0].data.map(
-        () => Math.floor(Math.random() * 100)
-      )
+      this.getChartData()
     }, 1700)
   },
-  beforeDestroy() {
+  beforeDestroy () {
     clearInterval(this.interval)
+  },
+  methods: {
+    getChartData () {
+      axios.get('/api/dashboard/chart', { params: { id: 123 } })
+        .then(response => {
+          this.chartOption = {
+            title: {
+              text: 'ECharts 入门示例'
+            },
+            tooltip: {},
+            xAxis: {
+              data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+            },
+            yAxis: {},
+            series: [
+              {
+                name: '销量',
+                type: 'bar',
+                data: response.data
+              }
+            ]
+          }
+        })
+    }
   }
 }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
 module.exports = {
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        bypass: function (req, res) {
+          if (req.headers.accept.indexOf('html') !== -1) {
+            console.log('Skipping proxy for browser request.')
+            return '/index.html'
+          } else {
+            if (req.path === '/favicon.ico') return
+            const name = req.path.split('/api/')[1].split('/').join('_')
+            const mock = require(`./mock/${name}`)
+            const result = mock(req.method)
+            delete require.cache[require.resolve(`./mock/${name}`)]
+            return res.send(result)
+          }
+        }
+      }
+    }
+  },
   css: {
     loaderOptions: {
       less: {
